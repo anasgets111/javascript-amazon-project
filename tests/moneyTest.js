50b5703b5350e02a1e18a226e250e6bc2cf0d6ca
Anas Khalifa <anasgets111@gmail.com>
2024-02-17 00:21:13 +0200

    create automated tests

---
+++ tests/moneyTest.js
@@ -0,0 +1,29 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("test suite: formatCurrency");
+console.log("converts cents into dollars");
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("works with zero");
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("edge case of rounding up");
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+console.log("edge case of rounding down");
+if (formatCurrency(2000.04) === "20.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
